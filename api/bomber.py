91d558da17a979a5189c0e57c75b2528d0656ee7
talha441 <58416871+talha441@users.noreply.github.com>
2025-07-14 16:07:23 +0600

    Create bomber.py

---
+++ api/bomber.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data.decode('utf-8'))
+
+        phone = data.get('phone', 'unknown')
+
+        response = {
+            "message": f"SMS attack launched on {phone}"
+        }
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode())
